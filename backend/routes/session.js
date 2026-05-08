87c7ab8615ef08a5a41811d1fb0280492f425ea5
Aishwarya Kurakula <aishwaryakurakula2511@gmail.com>
2026-05-08 13:45:08 +0530

    Update session.js

--- backend/routes/session.js
+++ backend/routes/session.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router  = express.Router();
 const crypto  = require('crypto');
-const { randomUUID } = require('crypto');
 const { sessions } = require('../websocket/handler');
 
 router.post('/create', (req, res) => {
